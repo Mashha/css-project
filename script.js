152ad2ad482e964fa2378a4a775965678f6fac49
Masa Orel <orel.masha89@gmail.com>
2022-05-26 16:00:24 +0200

    fixed the mobile nav bar

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const navSlide = () => {
   const nav = document.querySelector('.nav-links')
 
   burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active')
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle("toggle");
   })
+
+
 }
 
 navSlide()
